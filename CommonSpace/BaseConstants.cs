973875e05e6076a81463f24c6ab98e1a0a0ab63e
Philip Quirke <philipquirkenz@gmail.com>
2023-08-05 21:41:32 +1200

    Add UseLegs and NumLegsShown

--- CommonSpace/BaseConstants.cs
+++ CommonSpace/BaseConstants.cs
@@ -6,7 +6,7 @@ namespace SkyCombGround.CommonSpace
     public class BaseConstants : GroundColors
     {
         // The current version of the code base.
-        public static string CodeVersion = "6.1";
+        public static string CodeVersion = "6.2";
 
 
         // Number of decimal places for commonly used types of data
