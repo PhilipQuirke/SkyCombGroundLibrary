28dd38a6c00698c1fe7d6ef62237d552e9bf7e46
Philip Quirke <philipquirkenz@gmail.com>
2024-08-19 14:55:12 +1200

    Show DronePath in Flights

--- CommonSpace/BaseConstants.cs
+++ CommonSpace/BaseConstants.cs
@@ -6,7 +6,7 @@ namespace SkyCombGround.CommonSpace
     public class BaseConstants : GroundColors
     {
         // The current version of the code base.
-        public static string CodeVersion = "7.0";
+        public static string CodeVersion = "8.0";
 
 
         // Number of decimal places for commonly used types of data
